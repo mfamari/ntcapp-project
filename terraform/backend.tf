0371ccbf16af6a06b78413ac908376e7d628ca7c
babually9 <babually9@gmail.com>
2025-10-09 14:44:44 -0400

    terraform CI/CD

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "w7-ma-terraform-bucket"
-    key          = "week10project/terraform.tfstate"
-    region       = "us-east-1"
-   
+    bucket = "w7-ma-terraform-bucket"
+    key    = "week10project/terraform.tfstate"
+    region = "us-east-1"
+
   }
 }
