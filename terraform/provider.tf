9185423ea6bd00d134c467690dfc38110ca30013
babually9 <babually9@gmail.com>
2025-10-10 12:07:08 -0400

    CI/CD terraform cost analysis

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = "us-east-1"
   default_tags {
     tags = {
-      Environment = var.environment
+    
       Project     = "ntcapp"
       ManagedBy   = "terraform"
       Repository  = "https://github.com/mfamari/ntcapp-project.git"
